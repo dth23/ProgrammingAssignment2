d64a6c9a37600386cdcb29bf40f3a211e6b24976
dth23 <jttb.henderson@gmail.com>
2014-08-22 16:23:57 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,12 @@
 ## These two functions work together to solve for and cache the inverse of a matrix. In so doing, 
-## the inverse does not need to be recalculated, if the input matrix is unchanged,if is needed again. The 
-## Th epreviously solved for inverse matrix is pulled from the cache.
+## the inverse does not need to be recalculated, if the input matrix is unchanged, when it is called again.  
+## The previously solved for inverse matrix is pulled from the cache.
 
-## makeCacheMatrix is a function that creates a special matrix that is a list containing a function to:
-## 1) set the value of the matrix; 
-## 2) get the value of the matrix;
-## 3) set the value of the matrix inverse; and
-## 4) get the value of the matrix inverse.
+## makeCacheMatrix is a function that creates a special vector that is a list containing a function to:
+## 1) set the matrix; 
+## 2) get the matrix;
+## 3) set the matrix inverse; and
+## 4) get the matrix inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -24,7 +24,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## Write a short comment describing this function
+## cacheSolve is a function that calculates the inverse of the arguement matrix by using the functions from the special vecor 
+## created with the makeCacheMatrix function. However, it first checks to see if the inverse matrix has already been 
+## calculated. If so, it gets the inverse natrix from the cache and skips the computation. Otherwise, it solves for 
+##the inverse of the matrix and sets it in the cache via the setinverse function.
 
 cacheSolve <- function(x, ...) {
         cacheSolve <- function(x, ...) {
